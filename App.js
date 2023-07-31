33ded2f6e8989ad3e58d0f8501777bc359580da6
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-07-30 22:11:52 -0300

    animations

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, Animated } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      larAnimada: new Animated.Value(150),
+      altAnimada: new Animated.Value(50)
+    }
+
+    Animated.timing(
+      this.state.larAnimada,
+      {
+        toValue: 250,
+        duration: 2000
+      }
+    ).start();
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Animated.View style={{
+          width: this.state.larAnimada,
+          height: this.state.altAnimada,
+          backgroundColor: '#4169E1',
+          justifyContent: 'center'
+        }}>
+          <Text style={{ color: '#FFF', textAlign: 'center', fontSize: 22 }}>Carregando...</Text>
+        </Animated.View>
+      </View>
+    )
+  }
+
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center'
+  }
+})
